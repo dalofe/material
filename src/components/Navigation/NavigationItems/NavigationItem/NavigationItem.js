4b1c8124e912af4400b28eb582bc3e5bde5639fa
Dario Lopez <dlopez@kooomo.com>
2020-08-14 15:42:16 +0200

    makeStyles fixed in NavigationItem

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,13 +3,20 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 //import ListItemText from "@material-ui/core/ListItemText";
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-const navigationItem = ( props ) => {
+const useStyles = makeStyles({
+    colorLight: {
+      color: '#FFF'
+    }
+  });
+
+const NavigationItem = ( props ) => {
+    const classes = useStyles();
 
     return (
         <ListItem button>
-            {/* <ListItemIcon className={props.class}>{props.children}</ListItemIcon> */}
-            <ListItemIcon>{props.children}</ListItemIcon>
+            <ListItemIcon className={classes.colorLight}>{props.children}</ListItemIcon>
             <NavLink
                 to={props.link}
                 exact={props.exact}>{props.text}</NavLink>
@@ -17,4 +24,4 @@ const navigationItem = ( props ) => {
     );
 };
 
-export default navigationItem;
+export default NavigationItem;
